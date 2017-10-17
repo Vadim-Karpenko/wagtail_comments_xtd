d033b7ba67413e071f8e7b9de803cf4bec39f188
André Karlsson <andre@sess.se>
2017-10-17 22:32:28 +0200

    missing html temlates in sdist package, changing MANIFEST

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='André Karlsson',
     license='GPLv3',
     author_email='andre.karlsson@protractus.se',
-    version='0.2.5',
+    version='0.2.6',
     url='https://github.com/joyider/wagtail_comments_xtd',
     classifiers=[
         "Environment :: Web Environment",
